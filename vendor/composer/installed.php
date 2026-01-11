5c67d2abad6f1e79986a13bd7d761a92e6a56882
Nazmul Hasan Robin <robin.sust08@gmail.com>
2026-01-11 22:18:40 +0600

    Vendor added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nhrrob/nhrrob-secure',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '595c002ac297042b3b27e1e2d56c53263801d620',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'nhrrob/nhrrob-secure' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '595c002ac297042b3b27e1e2d56c53263801d620',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'robthree/twofactorauth' => array(
+            'pretty_version' => 'v3.0.3',
+            'version' => '3.0.3.0',
+            'reference' => '85408c4e775dba7c0802f2d928efd921d530bc5b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../robthree/twofactorauth',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
