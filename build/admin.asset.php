76da8d26d23012400a74c6057617e0ebc00e0e77
Nazmul Hasan Robin <robin.sust08@gmail.com>
2026-01-12 13:58:46 +0600

    Added vulnerability checker feature

--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'bc28e80d9f93b543a843');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'e7d8ff9f081925a3237b');
