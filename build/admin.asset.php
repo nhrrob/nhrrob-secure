e84921ffa3e3e6176baa757e9a8d507126b950e6
Nazmul Hasan Robin <robin.sust08@gmail.com>
2026-01-17 01:54:10 +0600

    Version 1.2.0

--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '434ed392d926a78bea76');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '876f03e1a6accd816f8d');
